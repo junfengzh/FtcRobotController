03945b3b528ec15b79140a255e93bf474f187c86
junfengzh <showking@gmail.com>
2025-02-23 09:25:45 -0800

    set up nginx proxy for FTC dashboard

--- docker-dev.sh
+++ docker-dev.sh
@@ -5,3 +5,19 @@ docker run -it -v .:/home/mobiledevops/app mobiledevops/android-sdk-image bash
 # - ./gradlew clean assemblyRelease
 # - adb connect <host:port>
 # - adb install -r TeamCode/build/outputs/apk/release/TeamCode-release.apk
+
+# set up nginx proxy for ftc dashboard
+#
+# add custom location
+# ws:// -> wss://
+# :8000 -> /wss
+#
+# rewrite web socket url
+# location /dash {
+#   proxy_pass http://10.0.0.169:8043;
+#   proxy_set_header Accept-Encoding "";
+#   sub_filter_types application/javascript text/javascript;
+#   sub_filter 'ws://' 'wss://';
+#   sub_filter '{window.location.hostname}:8000' '{window.location.hostname}/wss';
+#   sub_filter_once off;
+# }
